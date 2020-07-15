2d73097d26d1a111e6063c7f1cbb6e0cdaae05f9
Abhishek Bhonsle <abhishek.bhonsle234@gmail.com>
2020-07-15 22:46:02 +0530

    Completed initial UI

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from config import Config
-
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+Bootstrap(app)
 
 
 from application import routes
